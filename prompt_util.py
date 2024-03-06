f865f27230649b7f7cc735c4c8121dcbf2992e43
Sneha-Shajee-Mohan <156137687+Sneha-Shajee-Mohan@users.noreply.github.com>
2024-03-06 15:57:01 -0500

    Update prompt_util.py

--- prompt_util.py
+++ prompt_util.py
@@ -1,4 +1,8 @@
 from prompt_toolkit import prompt
 
 def report_count():
-    return prompt('Enter the token to search for: ')
+    corpus_tokens = word_tokenize(corpus.lower())
+    text = nltk.Text(corpus_tokens)
+    search_token = token.lower()
+    token_count = corpus_tokens.count(search_token)
+    print(f"The term '{search_token}' shows up in the corpus {token_count} times.")
