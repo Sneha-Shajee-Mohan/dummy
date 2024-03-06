836c94171b74fa56dcc8fdcd30cb3307dd8fe000
Sneha-Shajee-Mohan <156137687+Sneha-Shajee-Mohan@users.noreply.github.com>
2024-03-06 15:45:49 -0500

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import PlaintextCorpusReader
+
+def report_count():
+  corpus_tokens = word_tokenize(corpus.lower())
+  text = nltk.Text(corpus_tokens)
+  search_token = token.lower()
+  token_count = corpus_tokens.count(search_token)
+  print(f"The term '{search_token}' shows up in the corpus {token_count} times.")
